feaf8e907a1db1055d91cf5debd3379576e004ed
Gustavo Isidoro <tartuzet@gmail.com>
2023-08-07 10:23:58 -0600

    se agrego drawer y se corrigieron versiones

--- App.js
+++ App.js
@@ -2,15 +2,16 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Saludar from './src/components/Saludar';
 import { NavigationContainer } from '@react-navigation/native';
-import NavigationStack from './src/navigation/NavigationStack';
-import NavigationTab from './src/navigation/NavigationTab';
-
+// import NavigationStack from './src/navigation/NavigationStack';
+// import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
 
 export default function App() {
   return (
     <NavigationContainer>
       {/* <NavigationStack /> */}
-      <NavigationTab/>
+      {/* <NavigationTab/> */}
+      <NavigationDrawer/>
     </NavigationContainer>
   );
 }
