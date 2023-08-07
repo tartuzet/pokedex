f960b70eea5c42ed6fccc33b52e3a83d74671603
Gustavo Isidoro <tartuzet@gmail.com>
2023-08-07 12:47:34 -0600

    se limpio y creo el pokedex pero tiene error

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
-
-import { StyleSheet, Text, View } from 'react-native';
-import Saludar from './src/components/Saludar';
 import { NavigationContainer } from '@react-navigation/native';
-// import NavigationStack from './src/navigation/NavigationStack';
-// import NavigationTab from './src/navigation/NavigationTab';
-import NavigationDrawer from './src/navigation/NavigationDrawer';
+import {Navigation} from './src/navigation/Navigation';
+
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <NavigationStack /> */}
-      {/* <NavigationTab/> */}
-      <NavigationDrawer/>
+      <Navigation />
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/navigation/Navigation.js
@@ -0,0 +1,20 @@
+import {createBottomTabNavigator} from  '@react-navigation/bottom-tabs';
+import AccountScreen from '../screens/Account';
+import FavoriteScreen from '../screens/Favorite';
+import PokedexScreen from '../screens/Pokedex';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+
+return (
+
+    <Tab.Navigator >
+        <Tab.Screen name='Account' component={AccountScreen} />
+        <Tab.Screen name='Favorite' component={FavoriteScreen} />
+        <Tab.Screen name='Pokedex' component={PokedexScreen} />
+    </Tab.Navigator>
+)
+
+}
