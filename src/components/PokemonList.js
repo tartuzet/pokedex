4d6538eeb1baaba57bfaf88678d6160f78921a2d
Gustavo Isidoro <tartuzet@gmail.com>
2023-08-11 13:32:04 -0600

    StyleSheet con Platform

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,4 +1,4 @@
-import { StyleSheet,ActivityIndicator,FlatList } from 'react-native'
+import { StyleSheet,ActivityIndicator,FlatList,Platform } from 'react-native'
 import React from 'react'
 import PokemonCard from './PokemonCard';
 
@@ -30,10 +30,11 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
+    marginTop: Platform.OS==="android" ? 30:0
   },
   spinner:{
     marginTop: 20,
-    marginBottom:60
+    marginBottom: Platform.OS==="android" ? 90:60
   }
 })
 
