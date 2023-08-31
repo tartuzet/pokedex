2d8c134bc34fe023b3e5aeb2353e73f109e7314d
Gustavo Isidoro <tartuzet@gmail.com>
2023-08-30 18:38:21 -0600

    Lista de Pokémon favoritos

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,21 +1,64 @@
-import { View, Text,Button } from 'react-native'
-import React from 'react'
+import {Text,SafeAreaView } from 'react-native'
+import {useFocusEffect} from '@react-navigation/native';
+import React,{useState,useEffect,useCallback} from 'react'
 import {getPokemonsFavoriteApi} from '../api/favorite';
+import {getPokemonDetailsByIdApi} from '../api/pokemon';
+import useAuth from '../hooks/useAuth'
+import PokemonList from '../components/PokemonList';
 
 
 export default function Favorite() {
 
-  const checkFavorites = async ()=> {
-    const response =  await getPokemonsFavoriteApi();
-    console.log(response);
+  const [pokemons, setPokemons] = useState([])
+  const {auth} = useAuth();
 
-  }
+
+  useFocusEffect(
+
+    useCallback(()=>{
+
+      if(auth){
+        (async()=>{
+          const response = await getPokemonsFavoriteApi();
+  
+          const pokemonsArray =[];
+  
+          console.log('response',response);
+  
+          for await (const id of response) {
+  
+            console.log(id);
+            const pokemonDetails = await getPokemonDetailsByIdApi(id);
+            pokemonsArray.push({
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              type: pokemonDetails.types[0].type.name,
+              order: pokemonDetails.order,
+              image: pokemonDetails.sprites.other["official-artwork"].front_default,
+            });
+          }
+  
+          setPokemons([...pokemons, ...pokemonsArray]);
+  
+          console.log(pokemons);
+
+        })()
+      }
+    },[auth])
+
+  )
+
+
 
 
   return (
-    <View>
-      <Text>Favorite</Text>
-      <Button title='Obtener Favoritos' onPress={checkFavorites}></Button>
-    </View>
+
+    !auth ? 
+    <Text>Usuario no logueado</Text> : 
+    <SafeAreaView>
+      <PokemonList 
+      pokemons={pokemons} 
+      />
+    </SafeAreaView>
   )
 }
